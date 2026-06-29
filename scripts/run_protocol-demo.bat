442e10885c0a01a46c41603e04a40c2b846dea52
slamdd <zhouyohu@163.com>
2026-06-29 14:23:20 +0800

    移除无用的git管理的文件

--- scripts/run_protocol-demo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set "PATH=D:/Qt/Qt5.14.2/5.14.2/msvc2017_64/bin;%PATH%"
-set "QT_PLUGIN_PATH=D:/Qt/Qt5.14.2/5.14.2/msvc2017_64/plugins"
-
-chcp 65001 >nul
-
-D:/sbb/etest-demo/build/ninja-debug/bin/protocol-demo.exe
